f904a37310938be6883db90c38d8644ca7df113d
Cserne <angeli.cserne@gmail.com>
2022-02-23 10:58:00 +0100

    Changes added.

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        // this.greet = function() {
+        //     console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+        // }
+    }
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`) //Ha ezt használom, akkor a fenti kikommentezett rész nem kell, és fordítva. Így szebb a szintaxis elvileg.
+    }
+}
+// const policeman = {
+//     rank: "vezérezredes",
+//     arrest: function() {
+//         console.log(`Mint ${this.rank} letartóztatom.`)
+//     }
+// }
+
+// const policeman2 = {
+//     rank: "őrnagy",
+//     arrest: function() {
+//         console.log(`Mint ${this.rank} letartóztatom.`)
+//     }
+// }
+
+class Policeman extends Person {
+    constructor(name, age, rank) {
+        super(name, age);
+        this.rank = rank;
+    }
+    arrest() {
+        console.log(`Mint ${this.rank} letartóztatom.`);
+    }
+}
+
+const policeman = new Policeman("John", 25, "vezérezredes");
+const policeman2 = new Policeman("Jack", 35, "őrnagy");
+
+policeman.greet()
+policeman.arrest()
+policeman2.greet()
+policeman2.arrest()
 // function Person(name, age) {
 //     this.name = name;
 //     this.age = age;
@@ -6,15 +49,6 @@
 //     }
 // }
 
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-        this.greet = function() {
-            console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
-        }
-    }
-}
 // person.prototype.greet = function() {
 //         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`) 
 //     }//Ez akkor kell, ha a fenti function personbe nem írom be a this.greet functiont.
@@ -23,8 +57,8 @@ const person4 = new Person("John", 25); //New nélkül undefinedot ad.
 // console.log(person4);
 const person5 = new Person("Jack", 35);
 
-person4.greet();
-person5.greet();
+// person4.greet();
+// person5.greet();
 
 // const person1 = {
 //     name: "John",
